958251cefcada176e9545cc9c599ad83f1a4b76f
2020371019 <163356085+2020371019@users.noreply.github.com>
2024-03-26 22:02:13 -0600

    Add files via upload

---
+++ auth.controller.js
@@ -0,0 +1,59 @@
+//importar modelo de datos User
+import Reader from '../models/Reader';
+import { signToken } from "../utils/token";
+import Librarians from "../models/Librarians";
+
+//Exportar las funciones de signUp y signIn
+export const signUp = async (req, res) => {
+  try {
+    const { readername, email, password, roles } = req.body;
+
+    // Crear un nuevo usuario
+    const newReader = new Reader({
+      readername,
+      email,
+      password: await Reader.encryptPassword(password),
+    });
+    //condicional para asignar roles en caso de que no se asignen roles
+    //se asigna un rol de usuario
+    if(req.body.roles){
+      const foundRoles = await Librarians.find({name: {$in: roles} });
+      newReader.roles = foundRoles.map(role => role.id);
+    }else{
+      const role = await Librarians.findOne({ name: "CommonReader"});
+      newReader.roles = [role.id];
+    }
+    
+    // Guardar el usuario en la base de datos
+    const savedReader = await newReader.save();
+    console.log(savedReader);
+    // Crear el token después de guardar el usuario
+    const token = signToken(savedReader);
+
+    // Responder al cliente con el token
+    res.status(200).json({ status: 200, message:"Tu Usuario ha sido registrado" } );
+  } catch (error) {
+    console.error("Error al registrar usuario:", error);
+    res.status(500).json({ error: "Error al registrar usuario" });
+  }
+};
+
+//funcion para iniciar sesion 
+export const signIn = async (req, res) => {
+    const readerFound = await Reader.findOne({email: req.body.email}).populate("roles");
+    //si no se encuentra el usuario mandar mensaje de error
+    if(!readerFound) return res.status(400).json({message:"Usuario no encontrado"});
+    
+    const matchPassword = await Reader.comparePassword(req.body.password, readerFound.password);
+
+    if(!matchPassword) return res.status(401).json({token: null, message: "contra invalida"});
+    
+    //generar un token
+    const token = signToken({id: readerFound._id}, process.env.SECRET, {
+      expiresIn: 86400
+    });
+    //Mostrar usuario encontrado
+    //console.log(userFound);
+    //Enviar status y el token en la respuesta
+    res.status(200).json({token});
+}
