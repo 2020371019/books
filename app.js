cba90ec1ad261b3f3fc22abb995467394956017a
2020371019 <163356085+2020371019@users.noreply.github.com>
2024-04-03 15:19:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import libroutes from './src/routes/books.routes';
 import authRoutes from './src/routes/getin.routes';
 import createLibrarians from './src/libs/initialSetup';
 import express from 'express';
+import cors from 'cors';
 const app = express();
 //ejecutar la funcion por defecto
 createLibrarians();
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('welcome to bookstore ');
