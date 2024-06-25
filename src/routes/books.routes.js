e3ea789823b94e62e59d00ec6271922eb0332aa3
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-06-24 18:10:25 -0600

    Backend:Rutas_con_uso_del_token

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -6,16 +6,18 @@ import { authJwt } from "../middlewares";
 
 //Establecer ruta products mediante el metodo GET
 router.get('/', Book.getBook); // todos
-//router.post('/', [authJwt.verifyToken,authJwt.isMasterLibrarian], Book.createBook); //admin
 router.post('/', Book.createBook); //admin
 router.get('/:bookId', Book.getBookById); //todos
 router.get('/getbygenre/:genre', Book.getBooksByGenre); //todos
 router.post('/getbyauthor/', Book.getBooksByAuthor); //todos
-//router.put('/:bookId', [authJwt.verifyToken,authJwt.isMasterLibrarianOrPageGuardian], Book.updateBookById);//admin y moderator
 router.put('/:bookId', Book.updateBookById);//admin y moderator
-
-//router.delete('/:bookId', [authJwt.verifyToken, authJwt.isMasterLibrarianOrPageGuardian ], Book.deleteBookById);//admin y moderator
 router.delete('/:bookId', Book.deleteBookById);//admin y moderator
+
+//Haciendo uso de los tokens 
+router.delete('/del/:bookId', [authJwt.verifyToken, authJwt.isMasterLibrarianOrPageGuardian ], Book.deleteBookById);//admin y moderator
+router.post('/create', [authJwt.verifyToken,authJwt.isMasterLibrarian], Book.createBook); //admin
+router.put('/mod/:bookId', [authJwt.verifyToken,authJwt.isMasterLibrarianOrPageGuardian], Book.updateBookById);//admin y moderator
+
 
 
 export default router;
