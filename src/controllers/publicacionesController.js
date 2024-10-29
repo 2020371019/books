eb252e52a8ad0371958f8b374979cc8d3ee5d497
2020371019 <2020371019@uteq.edu.mx>
2024-10-28 18:39:58 -0600

    Se elimina userId al envio de la publicacion

--- src/controllers/publicacionesController.js
+++ src/controllers/publicacionesController.js
@@ -12,18 +12,16 @@ export const getPublication = async (req, res) => {
 
 export const createPublication = async (req, res) => {
     try {
-        const { titulo, author, description, reader } = req.body;
+        const { titulo, description } = req.body;
 
         // Verifica que todos los campos necesarios estén presentes
         if (!reader) {
             return res.status(400).json({ message: "El campo 'reader' es obligatorio" });
         }
-
         const newPublication = new Publicacion({
             titulo,
-            author,
             description,
-            reader // Agrega el ID del lector
+            // reader 
         });
 
         const publicationSaved = await newPublication.save();
@@ -42,3 +40,28 @@ export const getpublicationById = async (req, res) => {
         res.status(500).json({message: "La publicacion no Existe"});
     }
 }
+
+
+
+// export const createPublication = async (req, res) => {
+//     try {
+//         const { titulo, author, description, reader } = req.body;
+
+//         // Verifica que todos los campos necesarios estén presentes
+//         if (!reader) {
+//             return res.status(400).json({ message: "El campo 'reader' es obligatorio" });
+//         }
+
+//         const newPublication = new Publicacion({
+//             titulo,
+//             author,
+//             description,
+//             reader // Agrega el ID del lector
+//         });
+
+//         const publicationSaved = await newPublication.save();
+//         res.status(201).json({ message: "Publicación subida con éxito", publication: publicationSaved });
+//     } catch (error) {
+//         res.status(500).json({ message: "Ocurrió un error, la publicación no se creó", error: error.message });
+//     }
+// }
--- src/models/Publicacion.js
+++ src/models/Publicacion.js
@@ -2,13 +2,12 @@ import { Schema, model } from 'mongoose';
 
 const publicacionSchema = new Schema({
     titulo: String,
-    author: String,
     description: String,
-    reader: {
-        type: Schema.Types.ObjectId,
-        ref: 'Reader', 
-        required: true 
-    }
+    // reader: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'Reader', 
+    //     required: true 
+    // }
 }, {
     timestamps: true,
     versionKey: false
